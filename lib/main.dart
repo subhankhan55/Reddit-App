631ea1b679143db56abedc85621fd7d4a700b4a7
CIIT/FA23-BSE-093/WAH MUHAMMAD SUBHAN <fa23-bse-093@cuiwah.edu.pk>
2025-12-07 00:24:19 +0500

    Landing Page UI added - Subhan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_app/features/auth/screen/login_screen.dart';
 import 'package:reddit_app/theme/pallete.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: Pallete.darkModeAppTheme,
-      home: const SizedBox(),
+      home: const LoginScreen(),
     );
   }
 }
