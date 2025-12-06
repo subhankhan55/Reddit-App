de7434a4a3aa3c53e8d00a60aeefc2c181b2ada2
CIIT/FA23-BSE-093/WAH MUHAMMAD SUBHAN <fa23-bse-093@cuiwah.edu.pk>
2025-12-07 01:33:22 +0500

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:reddit_app/features/auth/screen/login_screen.dart';
 import 'package:reddit_app/theme/pallete.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );  
+runApp(const MyApp());
   runApp(const MyApp());
 }
 
