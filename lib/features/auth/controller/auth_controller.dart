1d80c569c39b7361c339d71954ac5c34cd9e1cca
Saif Ullah <157057484+saifu11ah007@users.noreply.github.com>
2025-12-08 05:42:58 +1100

    Auth Controller updated

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,4 +1,11 @@
+import 'package:reddit_app/features/auth/repository/auth_repository.dart';
+
+final AuthControllerProvider = Provider((ref)=>AuthController(authRepository: ref.read(authRepositoryProvider),),);
+
 class AuthController {
   final AuthRepository _authRepository;
   AuthController({required AuthRepository authRepository)} : _authRepository = authRepository;
+  void signWithGoogle(){
+    _authRepository.signWithGoogle(); 
+  }
 }
