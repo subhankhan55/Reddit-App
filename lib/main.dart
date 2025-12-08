a74e6b23dedbf016818e6a2f1ef8a6360515af4f
CIIT/FA23-BSE-093/WAH MUHAMMAD SUBHAN <fa23-bse-093@cuiwah.edu.pk>
2025-12-08 22:52:04 +0500

    minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_app/features/auth/screen/login_screen.dart';
+import 'package:reddit_app/router.dart';
 import 'package:reddit_app/theme/pallete.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:routemaster/routemaster.dart';
 import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,8 @@
 //log out
+import 'package:flutter/material.dart';
+import 'package:reddit_app/features/auth/screen/login_screen.dart';
+import 'package:routemaster/routemaster.dart';
+
 final loggedOutRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: LoginScreen())
   });
