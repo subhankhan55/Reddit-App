888b1ca5d60c2fa36a1e54c05ccd7b5acd11750c
Saif Ullah <157057484+saifu11ah007@users.noreply.github.com>
2025-12-09 05:42:21 +1100

    Refactor AuthController to include Ref parameter

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,19 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:reddit_app/features/auth/repository/auth_repository.dart';
-
+import 'package:reddit_app/models/user_model.dart';
+import 'package:riverpod/src/framework.dart';
+final userProvider=StreamProvider<UserModel?>((ref)=>Stream.value(null));
 final authControllerProvider = Provider(
   (ref) => AuthController(
-    authRepository: ref.read(AuthRepositoryProvider),
+    authRepository: ref.read(AuthRepositoryProvider), ref: ref,
   ),
 );
+final authStateChangesProvider = StreamProvider(
+    (ref) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.getUserData(ref.watch(userProvider).value!.uid);
+}
+);
 
-class AuthController {
+final getUserDataProvider=StreamProvider.family((ref, String uid){
+  final authController=ref.watch(authControllerProvider.notifier);
+  return authController.getUserData(uid);
+});
+
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
+  final Ref _ref;
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository, _ref = ref, super(false) ;
 
-  AuthController({required AuthRepository authRepository})
-      : _authRepository = authRepository;
+  Stream<User?> get authStateChanges => _authRepository.authStateChanges;
 
-  void signInWithGoogle() {
+  void signInWithGoogle(BuildContext context) async{
+    state=true;
     _authRepository.signInWithGoogle();
+    state=false;
+  }
+    Stream<UserModel> getUserData(String uid) {
+    return _authRepository.getUserData(uid);
   }
 }
