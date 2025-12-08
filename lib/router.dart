a4e3f95ed660cba018e1e67105de2304fc4b66df
Saif Ullah <157057484+saifu11ah007@users.noreply.github.com>
2025-12-09 05:43:48 +1100

    Add loggedInRoute for authenticated users

--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,13 @@
 //log out
 import 'package:flutter/material.dart';
 import 'package:reddit_app/features/auth/screen/login_screen.dart';
+import 'package:reddit_app/features/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: LoginScreen())
   });
 //login
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen())
+  });
